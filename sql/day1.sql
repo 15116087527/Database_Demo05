4d61987af66bd9f20d5c27d88059d63a6cd6e429
thu <mingfei.net@gmail.com>
2016-05-14 16:09:41 +0800

    Update.

--- sql/day1.sql
+++ sql/day1.sql
@@ -8,20 +8,23 @@ USE demo;
 DROP TABLE IF EXISTS demo.user;
 CREATE TABLE demo.user (
   id       INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- unsigned 无符（负）号
-  username VARCHAR(255),
-  password VARCHAR(255),
-  sex      CHAR(1) DEFAULT 'M', -- M:male; F:female 定长？
-  age      INT(3),
+  username VARCHAR(255) UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  sex      CHAR(1)          DEFAULT 'M', -- M:male; F:female 定长？
+  age      INT(3) CHECK (age > 18),
   height   DOUBLE(3, 2), -- 1.75 m
   salary   DECIMAL(6, 2), -- 1234.56
   birthday DATE
 );
 
-INSERT INTO demo.user VALUES (1, 'tester', '123', 'female', 20, 1.6, 6000, '1996-1-2');
-INSERT INTO demo.user VALUES (2, 'new user', '123', 'female', 20, 1.6, 6000, '1996-1-2');
-INSERT INTO demo.user VALUE (NULL, NULL , '123', 'female', 20, 1.6, 6000, '1996-1-2');
-
-INSERT INTO demo.user(id, username) VALUE(NULL , 'tester');
-
+INSERT INTO demo.user VALUES (NULL, NULL, '123', 'F', 16, 1.6, 6000, '1996-1-2');
 SELECT *
 FROM demo.user;
+
+DELETE FROM demo.user WHERE id = 5;
+
+TRUNCATE TABLE demo.user;
+
+-- student table
+# sno 2007220647 2003
+
