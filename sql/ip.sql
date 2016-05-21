1c6a6c366019361bff00b658a602dbb0f204a66d
thu <mingfei.net@gmail.com>
2016-05-21 11:58:17 +0800

    Update.

---
+++ sql/ip.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE db_ip;
+
+CREATE TABLE db_ip.ip (
+  id     INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  start  CHAR(15),
+  end    CHAR(15),
+  adress VARCHAR(255)
+);
+
+SELECT *
+FROM db_ip.ip;
+
+-- IPV4 111.111.111.111
---
+++ src/demo/DumpIp.java
@@ -0,0 +1,120 @@
+package demo;
+
+import com.mysql.jdbc.Driver;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mingfei.net@gmail.com
+ * 2016/5/21.
+ */
+public class DumpIp {
+
+    private static final String FILE = "data/ip.txt";
+    private static final java.lang.String URL = "jdbc:mysql:///db_ip?user=root&password=123456";
+    private static final java.lang.String SQL = "INSERT INTO db_ip.ip VALUES(NULL, ?, ?, ?)";
+    private static List<Ip> ips;
+
+    private static void parse() {
+        ips = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE))) {
+            String line, start, end, address;
+            while ((line = bufferedReader.readLine()) != null) {
+                start = line.split("\\s+")[0];
+                end = line.split("\\s+")[1];
+                address = line.replaceAll(start, "").replaceAll(end, "").trim();
+                Ip ip = new Ip(start, end, address);
+                ips.add(ip);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void dump() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            new Driver();
+            connection = DriverManager.getConnection(URL);
+            preparedStatement = connection.prepareStatement(SQL);
+            for (Ip ip : ips) {
+                preparedStatement.setString(1, ip.getStart());
+                preparedStatement.setString(2, ip.getEnd());
+                preparedStatement.setString(3, ip.getAddress());
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        parse();
+        long start = System.currentTimeMillis();
+        dump();
+        System.out.println((System.currentTimeMillis() - start) / 1000 + " seconds.");
+    }
+}
+
+class Ip {
+    private String start;
+    private String end;
+    private String address;
+
+    public Ip(String start, String end, String address) {
+        this.start = start;
+        this.end = end;
+        this.address = address;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
+
